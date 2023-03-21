7ced7d5b9c6a09fa04583eae9117785ea5b45cbd
Mike <lumen.net@yandex.ru>
2023-03-21 10:09:21 +0400

    Added a helper to determine the time difference

--- src/utils/dateTimeHelper.ts
+++ src/utils/dateTimeHelper.ts
@@ -8,22 +8,63 @@ import moment from "moment";
 /**
  * Date and time formatting.
  * 
- * {@link https://momentjs.com/docs/#/parsing/string-format/ Allowed formats}  
+ * {@link https://momentjs.com/docs/#/displaying/ Allowed formats}  
  * 
- * @param   {number | string | undefined | Date}    date    Date and time
+ * @param   {number | string | Date | undefined}    date    Date and time
  * @param   {string}    format      Template
  * @returns {string | undefined}    Formatting result
  */
 export const format = (
-    date: number | string | undefined | Date,
+    date: number | string | Date | undefined,
     format: string
 ): string | undefined => {
 
     if (!(typeof date === 'number' || typeof date === 'string' || date instanceof Date))
         return undefined;
 
-    if (!moment(date, true).isValid())
+    const dateMoment = moment(date, true);
+
+    if (!dateMoment.isValid())
         return undefined;
 
-    return moment(date).format(format);
+    return dateMoment.format(format);
+};
+
+
+/**
+ * Calculates the difference, in specified units, between two dates.
+ * 
+ * {@link https://momentjs.com/docs/#/durations/diffing/ Doc}  
+ * 
+ * @param   {number | string | Date | undefined}    firstDate     First date for comparison
+ * @param   {number | string | Date | undefined}    secondDate    Second date for comparison
+ * @param   {moment.unitOfTime.Diff}                [units]       What are the units to measure (default: minutes)
+ * @returns {string | undefined}    Difference in numerical form in the specified units of measurement
+ */
+export const diff = (
+    firstDate: number | string | Date | undefined,
+    secondDate: number | string | Date | undefined,
+    units: moment.unitOfTime.Diff = 'minutes'
+): number | undefined => {
+
+    // First date  
+    if (!(typeof firstDate === 'number' || typeof firstDate === 'string' || firstDate instanceof Date))
+        return undefined;
+
+    const firstDateMoment = moment(firstDate, true);
+
+    if (!firstDateMoment.isValid())
+        return undefined;
+
+    // Second date  
+    if (!(typeof secondDate === 'number' || typeof secondDate === 'string' || secondDate instanceof Date))
+        return undefined;
+
+    const secondDateMoment = moment(secondDate, true);
+
+    if (!secondDateMoment.isValid())
+        return undefined;
+
+    // Comparison 
+    return firstDateMoment.diff(secondDateMoment, units);
 }; 
