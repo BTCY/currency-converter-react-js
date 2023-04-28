2505355927f414fb529e8012078fd288b49d5d1c
Mike <lumen.net@yandex.ru>
2023-04-28 07:56:45 +0400

    Layout of the ExchangeRateHistoryResult page of results with charts

--- src/components/exchange-rate-history-tab/ExchangeRateHistoryResult.tsx
+++ src/components/exchange-rate-history-tab/ExchangeRateHistoryResult.tsx
@@ -6,7 +6,6 @@ import {
     Chart as ChartJS, CategoryScale, LinearScale,
     PointElement, LineElement, Title, Tooltip, Legend,
 } from 'chart.js'
-import { Table } from "react-bootstrap";
 
 ChartJS.register(
     CategoryScale,
@@ -24,6 +23,7 @@ interface IConversionResult {
 }
 
 interface IChart {
+    id: string;
     labels: string[];
     datasets: {
         label: string;
@@ -33,37 +33,30 @@ interface IChart {
     }[];
 }
 
-interface IApiResultData {
-    [key: string]: {
-        [key: string]: number
-    }
-}
-
-
-const getChartlabels = (
-    data: IApiResultData,
-): string[] => {
-    return Object.keys(data).map(d => d)
-}
-
-
 const getChartData = (
-    data: IApiResultData,
-    currCode: string,
-    curr: string,
-    labels: string[],
-): IChart => {
-    return ({
-        labels: labels,
-        datasets: [
-            {
-                label: `${currCode} (${curr})`,
-                backgroundColor: "rgb(255, 99, 132)",
-                borderColor: "rgb(255, 99, 132)",
-                data: Object.keys(data)?.map(d => data[d]?.[currCode]),
-            },
-        ],
-    })
+    apiResult: IApiExchangeRateHistory,
+    availableCurrencies: IApiAllAvailableCurrencies,
+): IChart[] => {
+
+    const charts: IChart[] = [];
+    const labels = Object.keys(apiResult.rates).map(d => d);
+
+    Object.keys(availableCurrencies?.symbols).map(currCode =>
+        charts.push({
+            id: currCode,
+            labels: labels,
+            datasets: [
+                {
+                    label: `${currCode} (${availableCurrencies?.symbols[currCode]})`,
+                    backgroundColor: "rgb(255, 99, 132)",
+                    borderColor: "rgb(255, 99, 132)",
+                    data: Object.keys(apiResult.rates)?.map(d => apiResult.rates[d]?.[currCode]),
+                },
+            ],
+        })
+    );
+
+    return charts;
 }
 
 
@@ -72,22 +65,20 @@ const ExchangeRateHistoryResult = ({
     availableCurrencies
 }: IConversionResult) => {
 
-    const labels = getChartlabels(result.rates)
+    const charts = getChartData(result, availableCurrencies);
+
     return (
         <>
             <h2>Base: {result.base} (<small>{availableCurrencies?.symbols[result.base]}</small>)</h2>
             <div>
-                {Object.keys(availableCurrencies?.symbols).map(currCode =>
-                    <div key={currCode}>
-                        <Line data={
-                            getChartData(
-                                result.rates,
-                                currCode,
-                                availableCurrencies?.symbols[currCode],
-                                labels
-                            )} />
-                    </div>
-                )}
+                {charts.length > 0
+                    ? charts.map(c =>
+                        <div key={c.id} className="w-50 d-inline-block p-4">
+                            <Line data={c} />
+                        </div>
+                    )
+                    : <p>No data</p>
+                }
             </div>
         </>
     )
