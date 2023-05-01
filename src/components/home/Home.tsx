2b2a15a2df709c6d514df0af09653533d4e7a794
Mike <lumen.net@yandex.ru>
2023-05-01 17:54:47 +0400

    Changed the approach to getting a list of currencies

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,8 +2,12 @@ import { Tab, Tabs } from 'react-bootstrap';
 import styles from './Home.module.css';
 import CurrencyConversionTab from '../currency-conversion-tab/CurrencyConversionTab';
 import CurrencyFluctuationsTab from '../currency-fluctuations-tab/CurrencyFluctuationsTab';
-import LatestExchangeRatesTab from '../latest-exchange-rates-tab/LatestExchangeRatesTab'; 
+import LatestExchangeRatesTab from '../latest-exchange-rates-tab/LatestExchangeRatesTab';
 import ExchangeRateHistoryTab from '../exchange-rate-history-tab/ExchangeRateHistoryTab';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../stores/hooks';
+import { availableCurrenciesThunk, selectAvailableCurrencies } from '../../stores/slices/currenciesSlice';
+import { shallowEqual } from 'react-redux';
 
 /**
  *   Home
@@ -11,41 +15,56 @@ import ExchangeRateHistoryTab from '../exchange-rate-history-tab/ExchangeRateHis
 
 const Home = () => {
 
+    const availableCurrencies = useAppSelector(selectAvailableCurrencies, shallowEqual);
+    const [availableCurrenciesIsLoading, setAvailableCurrenciesIsLoading] = useState<boolean>(true);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        setAvailableCurrenciesIsLoading(true);
+        dispatch(availableCurrenciesThunk())
+            .finally(() => setAvailableCurrenciesIsLoading(false));
+    }, [dispatch]);
+
     return (
         <div className={styles.tabsWrap}>
-            <Tabs
-                id="home-tab"
-                defaultActiveKey="converter"
-                justify
-            >
-                <Tab
-                    eventKey="converter"
-                    title="Converter"
-                >
-                    <CurrencyConversionTab />
-                </Tab>
+            {availableCurrenciesIsLoading && <>Loading...</>}
+            {!availableCurrenciesIsLoading && availableCurrencies?.symbols &&
+                <>
+                    <Tabs
+                        id="home-tab"
+                        defaultActiveKey="converter"
+                        justify
+                    >
+                        <Tab
+                            eventKey="converter"
+                            title="Converter"
+                        >
+                            <CurrencyConversionTab />
+                        </Tab>
 
-                <Tab
-                    eventKey="fluctuations"
-                    title="Fluctuations"
-                >
-                    <CurrencyFluctuationsTab />
-                </Tab>
+                        <Tab
+                            eventKey="fluctuations"
+                            title="Fluctuations"
+                        >
+                            <CurrencyFluctuationsTab />
+                        </Tab>
 
-                <Tab
-                    eventKey="latestExchangeRates"
-                    title="Latest Exchange Rates"
-                >
-                    <LatestExchangeRatesTab />
-                </Tab> 
-                
-                <Tab
-                    eventKey="exchangeRateHistory"
-                    title="Exchange Rate History"
-                >
-                    <ExchangeRateHistoryTab />
-                </Tab>
-            </Tabs>
+                        <Tab
+                            eventKey="latestExchangeRates"
+                            title="Latest Exchange Rates"
+                        >
+                            <LatestExchangeRatesTab />
+                        </Tab>
+
+                        <Tab
+                            eventKey="exchangeRateHistory"
+                            title="Exchange Rate History"
+                        >
+                            <ExchangeRateHistoryTab />
+                        </Tab>
+                    </Tabs>
+                </>
+            }
         </div>
     );
 }
