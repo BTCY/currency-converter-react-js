e479196b0dacfb0a210fc624930aa9ff26a8af49
Mike <lumen.net@yandex.ru>
2023-05-06 08:27:46 +0400

    Added Base Page Template

---
+++ src/page-templates/BasePageTemplate.tsx
@@ -0,0 +1,39 @@
+import Copyright from "../components/copyright/Copyright";
+import HeaderMenu from "../components/header-menu/HeaderMenu";
+import Logo from "../components/logo/Logo";
+import Body from "../layout/Body";
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+import Layout from "../layout/Layout";
+
+/**
+ *   Base page template
+ */
+
+interface IBasePageTemplate {
+    children?: React.ReactNode;
+}
+
+const BasePageTemplate = ({ children }: IBasePageTemplate) => {
+
+    return (
+        <Layout>
+
+            <Header>
+                <Logo />
+                <HeaderMenu />
+            </Header>
+
+            <Body>
+                {children && children}
+            </Body>
+
+            <Footer>
+                <Copyright />
+            </Footer>
+
+        </Layout>
+    );
+}
+
+export default BasePageTemplate;
