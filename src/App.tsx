6384020cdc62fac7ec7dd6a90bae176f99fef8fc
Mike <lumen.net@yandex.ru>
2023-05-06 12:24:38 +0400

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import { RoutesData } from './routes/routes';
 
-function App() {
-
-    return (
-        <Routes>
-            {/* data from ./routes/routes.tsx */}
-            {RoutesData && Object.keys(RoutesData).map((key: string) =>
-                <Route
-                    key={RoutesData[key].link}
-                    path={RoutesData[key].link}
-                    element={RoutesData[key].component}
-                />
-            )}
-        </Routes>
-        // <div className="App">
-        //     <header className="App-header">
-        //         <Counter />
-        //     </header>
-        // </div>
-    );
-}
+const App = () => (
+    <Routes>
+        {/* data from ./routes/routes.tsx */}
+        {RoutesData && Object.keys(RoutesData).map((key: string) =>
+            <Route
+                key={RoutesData[key].link}
+                path={RoutesData[key].link}
+                element={RoutesData[key].component}
+            />
+        )}
+    </Routes>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
