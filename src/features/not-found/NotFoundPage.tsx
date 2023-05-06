90b2d88f68d126287081aa294e4e896c077b8386
Mike <lumen.net@yandex.ru>
2023-05-06 07:15:32 +0400

    Styling 404 page

--- src/features/not-found/NotFoundPage.tsx
+++ src/features/not-found/NotFoundPage.tsx
@@ -21,7 +21,16 @@ const NotFoundPage = () => {
             </Header>
 
             <Body>
-                404
+                <div className="d-flex w-100 align-items-center justify-content-center">
+                    <div className="text-center">
+                        <h1 className="display-1 fw-bold">404</h1>
+                        <p className="fs-3">Page not found.</p>
+                        <p className="lead">
+                            The page you’re looking for doesn’t exist.
+                        </p>
+                        <a href="/" className="btn btn-primary">Go Home</a>
+                    </div>
+                </div>
             </Body>
 
             <Footer>
