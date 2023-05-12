1be65c2fe4234895131fb93b2d8365b5af488fdc
Mike <lumen.net@yandex.ru>
2023-05-12 07:41:02 +0400

    Added clear params button

--- src/components/common/tab-template/TabTemplate.tsx
+++ src/components/common/tab-template/TabTemplate.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, ReactElement } from "react";
 import styles from "./TabTemplate.module.css";
+import { Button } from "react-bootstrap";
+import { XLg } from "react-bootstrap-icons";
 
 /**
  *  Template for tab
@@ -12,16 +14,40 @@ interface ITabTemplate {
 
     /** Tab content */
     children?: ReactNode;
+
+    /** Hendle reset button */
+    handleResetButton?: () => void;
+
+    /** Disabled reset button */
+    isDisabledResetButton?: boolean;
 }
 
 
 const TabTemplate = ({
     title = undefined,
     children = undefined,
+    handleResetButton = undefined,
+    isDisabledResetButton,
 }: ITabTemplate): ReactElement => (
 
     <div className={styles.tabTemplateWrap}>
-        {title && <h2 className={styles.h2}>{title}</h2>}
+
+        <div className="d-flex justify-content-between mb-5"> 
+            {title && <h2 className="m-0 me-3">{title}</h2>}
+
+            {handleResetButton &&
+                <Button
+                    variant="link"
+                    title="Reset params"
+                    data-bs-toggle="tooltip" data-bs-placement="top"
+                    disabled={isDisabledResetButton}
+                    className="d-flex align-items-center p-1 mt-1"
+                >
+                    <XLg onClick={() => handleResetButton()} size={24} />
+                </Button>
+            } 
+        </div>
+
         {children && children}
     </div>
 );
