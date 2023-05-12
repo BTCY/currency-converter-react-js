b4251145280f7907961f96116d829a88cec58be5
Mike <lumen.net@yandex.ru>
2023-05-12 08:17:59 +0400

    Added a warning that IndexedDB is not working

--- src/components/home/HomeTabs.tsx
+++ src/components/home/HomeTabs.tsx
@@ -1,10 +1,11 @@
-import { Tab, Tabs } from "react-bootstrap";
+import { Alert, Tab, Tabs } from "react-bootstrap";
 import { ReactElement, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../stores/hooks";
 import { selectAvailableCurrencies } from "../../stores/currencies-slice/currenciesSlice";
 import { shallowEqual } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { availableCurrenciesThunk } from "../../stores/currencies-slice/availableCurrenciesThunk";
+import { RoutesData } from "../../routes/routes";
 import styles from "./HomeTabs.module.css";
 import CurrencyConversionTab from "./currency-conversion-tab/CurrencyConversionTab";
 import CurrencyFluctuationsTab from "./currency-fluctuations-tab/CurrencyFluctuationsTab";
@@ -12,12 +13,10 @@ import LatestExchangeRatesTab from "./latest-exchange-rates-tab/LatestExchangeRa
 import ExchangeRateHistoryTab from "./exchange-rate-history-tab/ExchangeRateHistoryTab";
 import DelayedSpinner from "../common/delayed-spinner/DelayedSpinner";
 import StubNoData from "../common/stub-no-data/StubNoData";
-import { RoutesData } from "../../routes/routes";
 
 /**
  *  Home content
  */
-
 
 enum TABS_ID {
     Converter = "converter",
@@ -25,6 +24,7 @@ enum TABS_ID {
     LatestExchangeRates = "latest-exchange-rates",
     ExchangeRateHistory = "exchange-rate-history",
 }
+
 
 const HomeTabs = (): ReactElement => {
 
@@ -62,10 +62,17 @@ const HomeTabs = (): ReactElement => {
             }
 
             {/* Data not loaded */}
-            {!availableCurrenciesIsLoading && !availableCurrencies?.symbols && <StubNoData />}
+            {!availableCurrenciesIsLoading && !availableCurrencies?.data?.symbols && <StubNoData />}
+
+            {/* Warning if IndexedDB not work */}
+            {!availableCurrenciesIsLoading && !availableCurrencies?.isCachedInIndexedDB &&
+                <Alert variant={"warning"}>
+                    Warning! IndexedDB does not work in your browser. The result of some functions will not be cached locally.
+                </Alert>
+            }
 
             {/* Tabs */}
-            {!availableCurrenciesIsLoading && availableCurrencies?.symbols &&
+            {!availableCurrenciesIsLoading && availableCurrencies?.data?.symbols &&
                 <Tabs
                     id="home-tab"
                     activeKey={tabId}
